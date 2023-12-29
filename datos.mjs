fc73255b7f33b2bdef4b54eb09b1553411be39f6
Josecopro <juanbece2006@gmail.com>
2023-12-29 11:49:12 -0500

    Cambio en el fetch del api (Adicion de valor NO 3)

--- datos.mjs
+++ datos.mjs
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 
-const response = await fetch("https://www.datos.gov.co/resource/cb9c-h8sn.json?tipo=Adición en el valor");
+const response = await fetch("https://www.datos.gov.co/resource/cb9c-h8sn.json?descripcion=ADICIONAL No 3");
 const fetchData = await response.json();
 console.log(fetchData);
 export {fetchData};
