399823a8f49a436878c95d88e9fbbdc56eb5c41c
Josecopro <juanbece2006@gmail.com>
2024-01-04 22:06:00 -0500

    Filtrado de datos Antioquia

--- datos.mjs
+++ datos.mjs
@@ -1,6 +1,5 @@
 import fetch from "node-fetch";
 
-const response = await fetch("https://www.datos.gov.co/resource/cb9c-h8sn.json?descripcion=ADICIONAL No 3");
+const response = await fetch("https://www.datos.gov.co/resource/rpmr-utcd.json?departamento_entidad=Antioquia");
 const fetchData = await response.json();
-console.log(fetchData);
 export {fetchData};
